ee594ab98152ce639c27a600fc3153f91443bff3
Mainak Das <mainak97@outlook.com>
2021-12-21 14:20:49 +0530

    added 01-hello-world.c
    This is a hello world program in C

---
+++ 01-hello-world.c
@@ -0,0 +1,8 @@
+// Hello World program 
+#include <stdio.h>//This is the standard input/output library
+
+void main()//This is the main function
+{
+    printf("Hello World\n");//This is the output
+}
+
